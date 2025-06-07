98e596a1f7cc28dbd6bcf46c749b8d1c20a42abf
Hyune-c <choi8608@gmail.com>
2025-06-07 14:49:55 +0900

    [2-8] CoroutineScope에서의 자원 반환

--- src/main/kotlin/base/base_2/Main.kt
+++ src/main/kotlin/base/base_2/Main.kt
@@ -1,16 +1,61 @@
 package base.base_2
 
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+import kotlin.random.Random
+
+//fun main() = runBlocking {
+//    val job = launch {
+//        delay(1000L)
+//        println("world")
+//    }
+//
+//    println("hello")
+//    job.join()  // Wait for the coroutine to finish
+//    println("done")
+//}
+
+//fun main() = runBlocking {
+//    doWork()
+//}
+//
+//suspend fun doWork() = coroutineScope {
+//    launch {
+//        delay(1000L)
+//        println("world")
+//    }
+//
+//    println("hello")
+//}
 
 fun main() = runBlocking {
-    val job = launch {
-        delay(1000L)
-        println("world")
+    val mock = listOf(1, 2, 3, 4, 5)
+    try {
+        val result = supervisorScope {
+            mock.map {
+                async {
+                    fetch(it)
+                }
+            }.mapNotNull { deferred ->
+                try {
+                    deferred.await()
+                } catch (e: Exception) {
+                    // todo 자원 반환
+                    println("Error fetching: ${e.message}")
+                    null
+                }
+            }
+        }
+        println(result)
+    } catch (e: Exception) {
+        println(e.message)
     }
+}
 
-    println("hello")
-    job.join()  // Wait for the coroutine to finish
-    println("done")
+suspend fun fetch(id: Int): String {
+    delay(Random.nextLong(500, 1500))
+    if (Random.nextBoolean()) throw Exception("failed to fetch id: $id")
+    return "fetched id: $id"
 }
